823723d741d71764c10b5a4cd49263093581675b
xuyao <xuyao0@foxmail.com>
2023-03-15 21:04:39 +0800

    test

--- src/test/java/com/xuyao/chat/ChatApplicationTests.java
+++ src/test/java/com/xuyao/chat/ChatApplicationTests.java
@@ -1,13 +1,25 @@
 package com.xuyao.chat;
 
+import com.xuyao.chat.bean.vo.Result;
+import com.xuyao.chat.bean.vo.Results;
+import com.xuyao.chat.service.SegmentService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import javax.annotation.Resource;
+
+@SpringBootTest(classes = ChatApplication.class)
 class ChatApplicationTests {
+
+    @Resource
+    private SegmentService segmentService;
 
     @Test
     void contextLoads() {
+        Result userId = segmentService.getId("userId");
+        System.out.println(userId);
+        Results userIds = segmentService.getIdList("userId", 10);
+        System.out.println(userIds);
     }
 
 }
